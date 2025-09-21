e7306098e84324ca6efd7a5dbc6c8f2c4db8cb9a
touyou <465697+touyou@users.noreply.github.com>
2025-09-21 13:42:01 +0900

    Initial Commit

---
+++ AddToAppSample/AddToAppSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AddToAppSampleApp.swift
+//  AddToAppSample
+//
+//  Created by 藤井陽介 on 2025/09/21.
+//
+
+import SwiftUI
+
+@main
+struct AddToAppSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AddToAppSampleTests/AddToAppSampleTests.swift
@@ -0,0 +1,16 @@
+//
+//  AddToAppSampleTests.swift
+//  AddToAppSampleTests
+//
+//  Created by 藤井陽介 on 2025/09/21.
+//
+
+import Testing
+
+struct AddToAppSampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
