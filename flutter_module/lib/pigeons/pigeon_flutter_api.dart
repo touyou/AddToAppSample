9ff27e03dcdf3bfd8aa6d3f996efaadf78acefdb
touyou <465697+touyou@users.noreply.github.com>
2025-09-26 00:25:25 +0900

    Pigeon統合のためのAPIファイルを生成し、依存関係を追加

---
+++ flutter_module/lib/pigeons/pigeon_flutter_api.dart
@@ -0,0 +1,14 @@
+import 'package:pigeon/pigeon.dart';
+
+@ConfigurePigeon(PigeonOptions(
+  dartOut: 'lib/pigeons/pigeon_flutter_api.g.dart',
+  dartOptions: DartOptions(),
+  swiftOut: '../App/AddToAppSample/Flutter/PigeonFlutterApi.swift',
+  swiftOptions: SwiftOptions(
+    includeErrorClass: false,
+  ),
+))
+
+@FlutterApi()
+abstract class TaskFlutterApi {
+}
---
+++ flutter_module/lib/pigeons/pigeon_host_api.dart
@@ -0,0 +1,34 @@
+import 'package:pigeon/pigeon.dart';
+
+@ConfigurePigeon(PigeonOptions(
+  dartOut: 'lib/pigeons/pigeon_host_api.g.dart',
+  dartOptions: DartOptions(),
+  swiftOut: '../App/AddToAppSample/Flutter/PigeonHostApi.swift',
+  swiftOptions: SwiftOptions(),
+))
+
+class Item {
+  Item({
+    required this.id,
+    required this.title,
+    this.isDone = false,
+    this.isFavorite = false,
+  });
+
+  final int id;
+  final String title;
+  final bool isDone;
+  final bool isFavorite;
+}
+
+@HostApi()
+abstract class TaskHostApi {
+  @async
+  List<Item> getItems();
+
+  @SwiftFunction('toggleFavorite(id:isFavorite:)')
+  void toggleFavorite(int id, bool isFavorite);
+
+  @SwiftFunction('toggleDone(id:isDone:)')
+  void toggleDone(int id, bool isDone);
+}
