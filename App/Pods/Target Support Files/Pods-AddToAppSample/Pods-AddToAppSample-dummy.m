cd44c5bd92054109d80443619feb234864cdc675
touyou <465697+touyou@users.noreply.github.com>
2025-10-14 12:29:09 +0900

    Remove unused CocoaPods files and update Flutter dependencies
    - Deleted unnecessary framework output files, scripts, and configuration files related to CocoaPods.
    - Updated Flutter version in mise.toml from 3.35.4-stable to 3.35.6-stable.
    - Added adaptive_platform_ui package with version 0.1.5+1 to pubspec.yaml and pubspec.lock.

--- App/Pods/Target Support Files/Pods-AddToAppSample/Pods-AddToAppSample-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AddToAppSample : NSObject
-@end
-@implementation PodsDummy_Pods_AddToAppSample
-@end
--- App/Pods/Target Support Files/cupertino_native/cupertino_native-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_cupertino_native : NSObject
-@end
-@implementation PodsDummy_cupertino_native
-@end
